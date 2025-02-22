ccd25534c03a320dff991e05478b962ea4c3b6df
Matthias Wild <matthias@wildpeter.de>
2025-02-22 10:08:06 +0100

    Text und Bilderkennung verbessert

--- lib/question_choice.py
+++ lib/question_choice.py
@@ -2,6 +2,43 @@ import math
 import cv2
 import numpy as np
 
+def find_circle(img, diameter):
+    radius = diameter / 2
+    h, w = img.shape[:2] # ist fuer orig_img identisch
+    if math.ceil(diameter) > h or math.ceil(diameter) > w:
+        raise Exception("Durchmesser ist groeser als Hoehe oder Weite des Bildes")
+    
+    for x_center in range(math.floor(radius), w - math.ceil(radius)):
+        for y_center in range(math.floor(radius), h - math.ceil(radius)):
+            notBlack = 0
+            for degree in range(0, 359):
+                theta = math.radians(degree)
+                x = round(x_center + radius * math.cos(theta))
+                y = round(y_center + radius * math.sin(theta))
+                if img[y, x] != 0:
+                    notBlack += 1
+
+            if notBlack < 10:
+                return x_center, y_center
+    
+    raise Exception("Kreis nicht gefunden")
+
+def get_black_ratio_in_circle(img, x_center, y_center, radius):
+    # Maske erstellen (schwarz = 0, weiß = 255)
+    mask = np.zeros_like(img, dtype=np.uint8)
+    cv2.circle(mask, (x_center, y_center), radius, 255, -1)
+
+    # Nur die Pixel innerhalb der Maske betrachten
+    masked_image = cv2.bitwise_and(img, img, mask=mask)
+
+    # Anzahl der schwarzen Pixel zählen (Pixelwert 0)
+    black_pixels = np.sum((masked_image == 0) & (mask == 255))
+
+    # Gesamte Pixelzahl berechnen
+    pixel_count = round(math.pi * radius**2)
+
+    return black_pixels / pixel_count
+
 def handle_multiple_choice(gray_img, orig_img, frage):
     result = {
         "frage": frage["wert"],
@@ -10,24 +47,31 @@ def handle_multiple_choice(gray_img, orig_img, frage):
 
     for checkbox in frage["checkboxes"]:
         # Nachdem es ein Rechteck ist, reichen je zwei x- und y-Werte
-        xl = math.floor(checkbox["x"]) # x Links
-        xr = math.floor(checkbox["x"] + frage["checkboxsize"]) # x rechts
-        yo = math.floor(checkbox["y"]) # y oben
-        yu = math.floor(checkbox["y"] + frage["checkboxsize"]) # y unten
+        outer_diameter = frage["checkboxsize"]
+        offset = round(outer_diameter) // 4
+        xl = math.floor(checkbox["x"] - offset) # x Links
+        xr = math.floor(checkbox["x"] + round(outer_diameter) + offset) # x rechts
+        yo = math.floor(checkbox["y"] - offset) # y oben
+        yu = math.floor(checkbox["y"] + round(outer_diameter) + offset) # y unten
         checkbox_part = gray_img[yo:yu, xl:xr]
-        
-        _, bw_img = cv2.threshold(checkbox_part, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        black_pixel_ratio = np.sum(bw_img == 0) / bw_img.size
-        if black_pixel_ratio > 0.6:
-            raise Exception("Checkbox \"" + checkbox["wert"] + "\" hat einen zu hohen Schwarzanteil")
-        elif black_pixel_ratio > 0.20:
+       
+        _, bw_img = cv2.threshold(checkbox_part, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        x_center, y_center = find_circle(bw_img, outer_diameter)
+
+        inner_radius = math.floor((outer_diameter - (outer_diameter / 10)) / 2)
+        black_pixel_ratio = get_black_ratio_in_circle(bw_img, x_center, y_center, inner_radius)
+        if black_pixel_ratio > 0.4:
+            orig_img = cv2.circle(orig_img, (x_center + xl, y_center + yo), inner_radius, (255, 0, 0), 2)
+            result["antwort"].append({"wert": checkbox["wert"], "checked": True})
+            print("Checkbox \"" + checkbox["wert"] + "\" hat einen sehr hohen Schwarzanteil")
+        elif black_pixel_ratio > 0.1:
             # checked
             result["antwort"].append({"wert": checkbox["wert"], "checked": True})
-            orig_img = cv2.rectangle(orig_img, (xl, yo), (xr, yu), (0, 255, 0), 2)
+            orig_img = cv2.circle(orig_img, (x_center + xl, y_center + yo), inner_radius, (0, 255, 0), 2)
         else:
             # not checked
             result["antwort"].append({"wert": checkbox["wert"], "checked": False})
-            orig_img = cv2.rectangle(orig_img, (xl, yo), (xr, yu), (0, 0, 255), 2)
+            orig_img = cv2.circle(orig_img, (x_center + xl, y_center + yo), inner_radius, (0, 0, 255), 2)
 
     return result, orig_img  
 
--- lib/question_text.py
+++ lib/question_text.py
@@ -4,6 +4,7 @@ from msrest.authentication import CognitiveServicesCredentials
 from dotenv import load_dotenv
 import os
 import cv2
+import numpy as np
 import io
 import time
 import math
@@ -46,17 +47,40 @@ def ocr(bw_img):
             return result
     else:
         raise Exception("Texterkennung nicht möglich")
+    
+def maskieren(img, orig_img, img_x, img_y, mask):
+    x = round(mask["x"])
+    y = round(mask["y"])
+    w = round(mask["w"])
+    h = round(mask["h"])
+    actual_x = x - img_x
+    actual_y = y - img_y
+
+    # Maske erstellen (schwarz = 0, weiß = 255)
+    mask = np.zeros_like(img, dtype=np.uint8)
+    cv2.rectangle(mask, (actual_x, actual_y), (actual_x + w, actual_y + h), 255, -1)
+    orig_img = cv2.rectangle(orig_img, (x, y), (x + w, y + h), (0, 0, 0), 2)
+
+    # Pixel der Maske weiß faerben
+    masked_img = cv2.bitwise_or(img, mask)
+    cv2.imwrite("debug/masked.jpg", masked_img)
+
+    return masked_img, orig_img
 
 def handle_text(gray_img, orig_img, frage):
     x1 = math.floor(frage["x"])
     x2 = math.floor(frage["x"] + frage["w"])
     y1 = math.floor(frage["y"])
     y2 = math.floor(frage["y"] + frage["h"])
-    cv2.rectangle(gray_img, (x1, y1), (x2, y2), 0, 2)
     textfeld = gray_img[y1:y2, x1:x2]
     orig_img = cv2.rectangle(orig_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
+    if "mask" in frage:
+        for mask in frage["mask"]:
+            textfeld, orig_img = maskieren(textfeld, orig_img, x1, y1, mask)
+
     text = ocr(textfeld).strip()
+    orig_img = cv2.putText(orig_img, text, (x1, y2), cv2.FONT_HERSHEY_SIMPLEX, (y2 - y1) // 50, (255, 0, 0), 2, cv2.LINE_AA)
     if re.match(frage["regex"], text):
         return {"frage": frage["wert"], "antwort": text}, orig_img
     else:
